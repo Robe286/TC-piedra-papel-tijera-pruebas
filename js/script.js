421ca94555bda43a62f9799e85095ef781b2186e
Roberto Blasco~ <roberza2202@gmail.com>
2025-03-14 20:21:36 +0100

    primerosPasos

---
+++ js/script.js
@@ -0,0 +1,31 @@
+/*## La estructura podría ser la siguiente:
+
+- Opciones en un array
+- Variables puntos usuario
+- Variables puntos ordenador
+- Capturar los botones para usarlos (plantea usar un forEach) y hacer evento click
+- Capturar resultados
+- Capturar contador-usuario
+- Capturar contador-ordenador
+- Crear una función obtenerResultado
+- Crear una función mostrarResultado
+- Crear una función actualizarPuntuacion*/
+
+
+
+const opciones = ['piedra', 'papel', 'tijera'];
+
+const puntosUsuario = document.getElementById('contador-usuario');
+const puntosOrdenador = document.getElementById('contador-ordenador');
+const botones = document.querySelectorAll('.boton-jugada');
+const resultados = document.getElementById('resultados');
+const contadorUsuario = document.getElementById('contador-usuario');
+const contadorOrdenador = document.getElementById('contador-ordenador');
+
+
+console.log(contadorOrdenador);
+
+
+
+
+
